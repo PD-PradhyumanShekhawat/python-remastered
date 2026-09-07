e67b552ba6c8a8013fa306c4ba9730df1c9a59ac
Pradhyuman Shekhawat <spradhyuman.shekhawat@gmail.com>
2026-09-06 23:07:52 -0400

    feat: add class and object fundamentals

---
+++ classes_and_objects.py
@@ -0,0 +1,16 @@
+class Car:
+
+    def drive(self):
+        print("The car is driving.")
+
+
+car1 = Car()
+car2 = Car()
+
+car1.drive()
+car2.drive()
+
+
+if __name__ == "__main__":
+    print(type(car1))
+    print(type(car2))
